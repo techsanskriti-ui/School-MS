a2136d4e7a1f625770a9d1c30045c602c35d3f66
techsanskriti-ui <techsanskriti87@gmail.com>
2025-08-05 10:57:13 +0530

    Added onthers  pages

--- userapp/apps.py
+++ userapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StudentConfig(AppConfig):
+class UserappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Student'
+    name = 'userapp'
